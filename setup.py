567d357099126e5e45a474884e5d2d16026a2c47
Lain Supe <lain.proliant@gmail.com>
2018-02-15 10:18:54 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.4.1',
+    version='2.4.2',
 
     description='The Python dependency injector from outer space.',
     long_description=long_description,
