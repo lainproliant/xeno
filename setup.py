122c55f1ec6e6756c09e4a5619ed65dc29e8a2fa
Lain Musgrove <lain.proliant@gmail.com>
2020-11-05 02:02:21 -0800

    Remove unneeded dependency.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="xeno",
-    version="4.4.0",
+    version="4.4.1",
     description="The Python dependency injector from outer space.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -28,7 +28,7 @@ setup(
     ],
     keywords="IOC dependency injector",
     packages=find_packages(),
-    install_requires=["ansicolors==1.1.8"],
+    install_requires=[],
     extras_require={},
     package_data={"xeno": ["LICENSE"]},
     data_files=[],
